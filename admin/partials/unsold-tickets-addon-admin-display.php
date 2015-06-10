46c777438afc675367e79d7093914ea29114af1d
Reydel Leon Machado <contact@reydelleon.me>
2015-06-10 11:08:44 -0400

    Creates new structure based on the WP Plugin Boilerplate. Refactors the comments, files and classes to fit the pluign name.

---
+++ admin/partials/unsold-tickets-addon-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://github.com/reydelleon/unsold-tickets-addon
+ * @since      0.1.0
+ *
+ * @package    unsold_tickets_addon
+ * @subpackage unsold_tickets_addon/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-unsold-tickets-addon-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/reydelleon/unsold-tickets-addon
+ * @since      0.1.0
+ *
+ * @package    unsold_tickets_addon
+ * @subpackage unsold_tickets_addon/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      0.1.0
+ * @package    unsold_tickets_addon
+ * @subpackage unsold_tickets_addon/includes
+ * @author     Reydel Leon Machado <contact@reydelleon.me>
+ */
+class Unsold_Tickets_AddOn_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    0.1.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
