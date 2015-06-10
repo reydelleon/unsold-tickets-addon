8276e6bc86bccdfbf04e6469bda32706a5504f50
Reydel Leon Machado <contact@reydelleon.me>
2015-06-10 10:35:48 -0400

    Deletes the current implementation to start anew with the WP Plugin Boilerplate.

--- views/wpps-settings/page-settings-section-headers.php
+++
@@ -1,9 +0,0 @@
-<?php if ( 'wptut_section-basic' == $section['id'] ) : ?>
-
-	<p>Example section introduction.</p>
-
-<?php elseif ( 'wptut_section-advanced' == $section['id'] ) : ?>
-
-	<p>Another example section introduction.</p>
-
-<?php endif; ?>
